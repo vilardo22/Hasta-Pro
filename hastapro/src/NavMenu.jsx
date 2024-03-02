aa753dc3a3667e621a8665a9a502beac05c97e35
Vilardo22 <gabrielvilardo22@gmail.com>
2024-03-02 17:22:07 -0300

    css

--- hastapro/src/NavMenu.jsx
+++ hastapro/src/NavMenu.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import './css/navmenu.css'
 
 const NavMenu = () => {
   return (
-    <div className='container3'>
-        <div className="container3-navegacao">
+    <div className='container-3'>
+        <div className="container-3-navegacao">
             <ul>
                 <li><a href="#Sobre">Sobre Nós</a></li>
+
                 <li><a href="#Sistema">Sistema</a></li>
+
                 <li><a href="#Resultados">Resultados</a></li>
             </ul>
         </div>
--- hastapro/src/Welcome.jsx
+++ hastapro/src/Welcome.jsx
@@ -4,12 +4,13 @@ import banner1 from './assets/banner1.jpg'
 
 const Welcome = () => {
   return (
-    <div className='container'>
+    <div className='container-1'>
        <div className="container-banner">
-        
-                <h1>Somos Hasta Pro</h1>
-             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea necessitatibus quas itaque corrupti iusto!</p>
-            <img src={banner1} alt="banner1" />
+          <div className="texto">
+          <h1>Somos Hasta Pro</h1>
+             <p>Lorem, ipsum dolor sit amet consectetur adipisicing !</p>
+          </div>
+                 <img src={banner1} alt="banner1" />
             
          </div>
        
