cf2c5c3fd8095ba18929514c06e0d4ec7094185c
Vilardo22 <gabrielvilardo22@gmail.com>
2024-03-09 14:49:34 -0300

    form

--- hastapro/src/Form.jsx
+++ hastapro/src/Form.jsx
@@ -3,19 +3,23 @@ import "./css/form.css"
 const Form = () => {
   return (
     <div className="container10">
-        <h1 className="titulo3">Prencha seus dados</h1>
+    <div className="forms">
+        <h2 className="titulo3">Prencha seus dados</h2>
             <p>Que entraremos em contato</p>
 
-            <form action="" method='post'className="formulario">
+            <form action="https://api.staticforms.xyz/submit" method='post'className="formulario">
                 <label>Nome</label>
                 <input type="text" name="name" placeholder='Digite seu nome'/>
                 <label>Email</label>
                 <input type="email" name="email" placeholder='Digite seu Email'/>
                 <label>Telefone</label>
-                <input type="number" name="number" placeholder='Digite seu Telefone'/>
+                <input type="text" name="phone" placeholder='Digite seu Telefone'/>
                 <button type='submit'>Enviar</button>
+                    <input type="hidden" name="accessKey" value="2da9703c-1dab-4c0e-a918-c52f12f54c78"></input>
+                    <input type="hidden" name="redirectTo" value="http://localhost:5173/"></input>
             </form>
     <div className="forms"></div>            
+    </div>
     </div>
   )
 }
